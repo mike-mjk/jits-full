16198a590b3158f73934eb54dd17b9eb273f19b3
mike-mjk <mikeavida@gmail.com>
2017-06-25 11:45:50 -0700

    mlab configuration

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
+                       //devquestion mlab password is password and out in the open
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://mikeavida:password@ds115071.mlab.com:15071/heroku_m7klh9np' :
+                            'mongodb://mikeavida:password@@ds137882.mlab.com:37882/heroku_qh2rggxx' :
                             'mongodb://localhost/jiu-jitsu-dev');
 exports.PORT = process.env.PORT || 8080;
