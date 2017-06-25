6d92ce9b4300dc1928667b41b4f5337cc4ebef85
mike-mjk <mikeavida@gmail.com>
2017-06-25 12:48:29 -0700

    heroku test

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -2,7 +2,7 @@ const jwt = require('jwt-simple');
 const User = require('../models/user');
 // const config = require('../secret');
 let config = null;
-(process.env.NODE_ENV === 'production' ? null : config = require('../secret'));
+(process.env.NODE_ENV === 'production' ? config = require('../fakeSecret') : config = require('../secret'));
 
 function tokenForUser(user) {
 	const timestamp = new Date().getTime();
