0af6e5042218a71a3517a50f8e2c9fabf747a924
mike-mjk <mikeavida@gmail.com>
2017-06-25 14:59:57 -0700

    fix typo

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://mikeavida:password@@ds137882.mlab.com:37882/heroku_qh2rggxx' :
+                            'mongodb://mikeavida:password@ds137882.mlab.com:37882/heroku_qh2rggxx' :
                             'mongodb://localhost/jiu-jitsu-dev');
 exports.PORT = process.env.PORT || 8080;
